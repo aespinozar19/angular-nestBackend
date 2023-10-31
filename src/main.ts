c8f82408ad0228f38188e41bd9725da5d6c921e7
aespinozar19 <aespinozar19@gmail.com>
2023-10-31 18:58:51 -0500

    change port log add

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,11 @@ async function bootstrap() {
     })
    );
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+
+  console.log(`App corriendo en puerto ${ port }`);
+
+  await app.listen( port );
 
   
 
